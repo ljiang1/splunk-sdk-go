1b8ef65a834078a3145d1e9d69acd23a4b446ce3
Liying Jiang <ljiang@splunk.com>
2018-03-02 10:54:23 -0800

    update

---
+++ hello.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+func main() {
+	var i int =0
+
+	fmt.Printf("hello world %d",i)
+}
+
+func avg(input1,input2  int) float32{
+
+	var x = input1+input2
+	return float32(x/2);
+}
